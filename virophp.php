c317617c04dea274912d2d48cf916098d5104915
HirschvoglLena <lena.hirschvogl@gmail.com>
2021-03-02 09:25:18 +0100

    phpfile

---
+++ virophp.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<style type="text/css">
+<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+</style>
+</head>
+<body>
+</body>
+</html>
+<?php phpinfo(); ?>
